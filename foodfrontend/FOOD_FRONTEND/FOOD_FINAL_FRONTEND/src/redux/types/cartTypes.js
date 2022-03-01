e0951d0d68d7c5cbdaedb35f1b788a61084b000c
shanmukh358 <85746652+shanmukh358@users.noreply.github.com>
2022-03-01 19:37:26 +0530

    Delete foodfrontend directory

--- foodfrontend/FOOD_FRONTEND/FOOD_FINAL_FRONTEND/src/redux/types/cartTypes.js
+++
@@ -1,5 +0,0 @@
-//customer food actions
-export const ADD_TO_CART = "add to cart";
-export const REMOVE_FROM_CART = "remove from cart";
-export const FETCH_CART = "fetch from cart";
-export const CHECKOUT = "check out";
